61beedc487ec8688b8961a48497824363203df90
deiivy <deivypr28@gmail.com>
2023-10-07 17:12:14 -0500

    mi primer proyecto con React

---
+++ src/data/vehicles.js
@@ -0,0 +1,28 @@
+ const vehicles = [
+
+ {
+    name: 'supra',
+    description: '300km/h',
+    image: 'https://images.pexels.com/photos/13627440/pexels-photo-13627440.jpeg?cs=srgb&dl=pexels-deane-bayas-13627440.jpg&fm=jpg'
+ },
+
+ {
+    name: 'gtr',
+    description: '280km/h',
+    image: 'https://www.elcarrocolombiano.com/wp-content/uploads/2021/04/Diseno-sin-titulo-4-4.jpg'
+ },
+
+ {
+    name: 'aventador',
+    description: '350km/h',
+    image: 'https://www.motor.com.co/__export/1677002784764/sites/motor/img/2023/02/21/https___www_carscoops_com_wp-content_uploads_2023_02_lamborghini-aventador-sv-1-1024x682.jpg_1902800913.jpg'
+ },
+
+ {
+    name: 'bugatti',
+    description: '500km/h',
+    image: 'https://www.elcarrocolombiano.com/wp-content/uploads/2023/02/02-02-2023-PORTADA-Bugatti-Chiron-Profilee-min.jpg'
+ }
+ ]
+
+ export default vehicles;
